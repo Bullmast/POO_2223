a390e459988933f19ca1639a959aaa440776fb95
DHighRunner <35639077+Bullmast@users.noreply.github.com>
2023-03-15 00:11:31 +0000

    Exercises not done on Telemóvel class... Carro barely started but with problems. LinhaDeEncomenda class done.

---
+++ src/Ficha3/Carro.java
@@ -0,0 +1,15 @@
+package Ficha3;
+
+public class Carro {
+
+    private String marca;
+    private String modelo;
+    private int anoConstrucao;
+    private double consumoPorKm;
+    private double kmsTotais;
+    private double mediaConsumo;
+    private double kmsLast;
+    private double mediaLast;
+
+
+}
